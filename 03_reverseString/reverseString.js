af7bdbfc4b0b7766ca36be629bf7d146d9f6b8d8
martinsBroks <martinsbroks14@gmail.com>
2022-08-30 12:11:37 +0300

    js excercises parctice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function(str) {
 
+//const myArray = string.split(""); //turn string into an array
+//const reversed = myArray.reverse() //revers array
+
+    
+    
+ 
+const reverseString = function(str) {
+    let reversed = '';
+    reversed = str.split("").reverse().join("") 
+    return reversed
+    
+    //turn back to string
 };
 
+console.log(reverseString("123hehe"))
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
 
-};
+/*
+
+const removeFromArray = function(...remove) {
+    const array = remove[0];
+    const removedArray = [];
+    
+    array.forEach((item) => {
+        if (!remove.includes(item)){
+         removedArray.push(item);
+    
+    
+    }});
+    return removedArray;
+}
+console.log(removeFromArray)([123], 2)
+*/
+const removeFromArray = function(...args) {
+       const array = args[0]
+       return array.filter(val => !args.includes(val))
+    }
+    
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
