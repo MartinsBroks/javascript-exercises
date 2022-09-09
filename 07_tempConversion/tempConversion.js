09a7dd471ca3e4e760b6f67be9f3301a4c736f7c
MartinsBroks <martinsbroks@gmail.com>
2022-09-09 12:24:43 +0300

    temp conversion excercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  c=(f-32)*(5/9)
+  cRounded = Math.round(c * 10) / 10
+  return cRounded
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  f=c*(9/5)+32
+  fRounded = Math.round(f * 10) / 10
+  return fRounded
 };
-
+console.log(ftoc(12))
+console.log(ctof(0))
 // Do not edit below this line
 module.exports = {
   ftoc,
